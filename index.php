c85ec2cd71b7495d8a25e62700c27196defbd92f
mkbyx <leo.caron@ynov.com>
2026-02-24 15:05:27 +0100

    .

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 require_once 'config/config.php';
 require_once 'controllers/MovieController.php';
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Content-Type: application/json");
 
+// ----------- ROUTES API -----------
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
-if ($method === 'GET' && str_starts_with($path, '/movies')) {
+if (str_starts_with($path, '/movies') && $method === 'GET') {
     $type = $_GET['type'] ?? 'popular';
     MovieController::list($type);
     exit;
@@ -19,21 +22,19 @@ if ($method === 'POST' && $path === '/favorites') {
     exit;
 }
 
-http_response_code(404);
-echo json_encode(["error" => "Route inconnue"]);
-
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-
-if ($uri === '/' || $uri === '/index') {
-    require './templates/header.php';
+// ----------- ROUTES FRONT HTML -----------
+if ($path === '/' || $path === '/index') {
+    header("Content-Type: text/html");
+    //require './templates/header.php';
     require './templates/home.php';
-} elseif ($uri === '/contact') {
-    echo "Page contact";
-} else {
-    http_response_code(404);
-    echo "Page introuvable";
+    exit;
 }
 
-?>
+if ($path === '/contact') {
+    header("Content-Type: text/html");
+    echo "Page contact";
+    exit;
+}
 
+http_response_code(404);
+echo json_encode(["error" => "Route inconnue"]);
--- services/TMDBService.php
+++ services/TMDBService.php
@@ -1,14 +1,17 @@
 <?php
 
+require_once __DIR__ . '/../config/config.php';
+
 class TMDBService {
 
-    public static function getMovies($type) {
+    public static function getMovies($type = 'popular') {
+
         $url = TMDB_BASE_URL . "/movie/$type?api_key=" . TMDB_API_KEY . "&language=fr-FR";
 
-        $response = @file_get_contents($url);
+        $response = file_get_contents($url);
 
         if (!$response) {
-            return ["error" => "Impossible d'appeler l'API TMDB"];
+            return ["error" => "Impossible de contacter l'API TMDB"];
         }
 
         return json_decode($response, true);
